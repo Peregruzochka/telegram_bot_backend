ab5e731e702797adfe20ac370c67cda059f799dc
Peregruzochka <peregruzochka48@mail.ru>
2025-01-10 15:42:04 +0300

    init

---
+++ src/main/java/ru/peregruzochka/telegram_bot_backend/TelegramBotBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.peregruzochka.telegram_bot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/peregruzochka/telegram_bot_backend/TelegramBotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.peregruzochka.telegram_bot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
