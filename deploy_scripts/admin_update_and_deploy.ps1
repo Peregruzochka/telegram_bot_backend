de3bf14aea8971b6b0fdc3c5a2ed39f85144dabc
Oleg Umrikhin <86581077+Peregruzochka@users.noreply.github.com>
2025-03-20 22:41:54 +0300

    изменил скрипты с помощью которых нужно поднимать прод

--- deploy_scripts/admin_update_and_deploy.ps1
+++ deploy_scripts/admin_update_and_deploy.ps1
@@ -1,7 +1,11 @@
 $path_to_service_name_dir = "C:\Users\Peregruzochka\Documents\GitHub\tg_bot_admin"
+$path_to_docker_compose_dir = "C:\Users\Peregruzochka\Documents\GitHub\telegram_bot_backend"
 $service_name = "admin"
+$compose_file = "docker-compose.prod.yaml"
 
 git -C $path_to_service_name_dir pull origin master
-docker-compose build $service_name
-docker-compose up -d --no-deps $service_name
+
+cd $path_to_docker_compose_dir
+docker-compose -f "$compose_file" build $service_name
+docker-compose -f "$compose_file" up -d --no-deps $service_name
 docker image prune -f
