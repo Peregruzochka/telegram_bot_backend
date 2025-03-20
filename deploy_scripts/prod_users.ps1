fb26a09667cdd57a7599aaf22655f6f63cf4e0f2
Oleg Umrikhin <86581077+Peregruzochka@users.noreply.github.com>
2025-03-21 00:56:37 +0300

    исправил продовские деплой скрипты

--- deploy_scripts/prod_users.ps1
+++ deploy_scripts/prod_users.ps1
@@ -5,6 +5,9 @@ $compose_file = "docker-compose.prod.yaml"
 
 git -C $path_to_service_name_dir pull origin master
 
+cd $path_to_docker_compose_dir
 docker-compose -f "$compose_file" build $service_name
 docker-compose -f "$compose_file" up -d --no-deps $service_name
 docker image prune -f
+
+Read-Host "Enter"
