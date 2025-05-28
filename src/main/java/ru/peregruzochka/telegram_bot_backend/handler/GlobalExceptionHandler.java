84097c8fab222a9488bcc3528e7aaa53adefd04c
Peregruzochka <peregruzochka48@mail.ru>
2025-05-28 22:27:29 +0300

    add controller advice

---
+++ src/main/java/ru/peregruzochka/telegram_bot_backend/handler/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package ru.peregruzochka.telegram_bot_backend.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorDetails.put("error", "Internal Server Error");
+        errorDetails.put("message", ex.getMessage());
+        errorDetails.put("exception", ex.getClass().getName());
+        errorDetails.put("path", "/registrations");
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
